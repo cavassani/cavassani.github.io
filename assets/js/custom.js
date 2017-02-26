860cf4acf51b019e8da19afd95e11df653a89a22
Marcelo  Cavassani <marceloscavassani@gmail.com>
2017-02-26 11:08:31 -0300

    testando formspree para envio de email no githubpages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -155,7 +155,8 @@
 			else {
 					 $.ajax({
 							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-							url         : 'assets/php/contact.php', // the url where we want to POST
+//							url         : 'assets/php/contact.php', // the url where we want to POST
+                            url         : "https://formspree.io/marceloscavassani@gmail.com", //mnda email pelo formspree
 							data        : formData, // our data object
 							dataType    : 'json', // what type of data do we expect back from the server
 							encode      : true,
